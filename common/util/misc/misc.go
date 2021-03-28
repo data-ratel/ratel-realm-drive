070a50e4ed040aa19572bc6645a3ca8829cfbf8c
Jin Qin <qin_jin@live.com>
2021-03-27 17:28:26 -0700

    update: remove useless codes

--- common/util/misc/misc.go
+++ common/util/misc/misc.go
@@ -4,8 +4,6 @@ import (
 	"os"
 )
 
-// var storageConfig = config.GetStorageConfig()
-
 func IsPathExists(path string) bool {
 	if _, err := os.Stat(path); err == nil {
 		return true
@@ -24,66 +22,3 @@ func IsPathDir(path string) (bool, error) {
 
 	return fi.IsDir(), nil
 }
-
-// func ZipFiles(sources []string, target string) error {
-// 	zipfile, err := os.Create(target)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	defer zipfile.Close()
-
-// 	// Create a new zip archive.
-// 	archive := zip.NewWriter(zipfile)
-// 	defer archive.Close()
-
-// 	rootDir := storageConfig.StorageRootDir
-
-// 	for _, path := range sources {
-// 		fullpath := rootDir + "/" + path
-// 		isDir, err := IsPathDir(fullpath)
-// 		if err != nil {
-// 			return err
-// 		}
-
-// 		if !isDir {
-// 			continue
-// 		}
-
-// 		baseDir := filepath.Base(fullpath)
-// 		filepath.Walk(fullpath, func(path string, info os.FileInfo, err error) error {
-// 			header, err := zip.FileInfoHeader(info)
-// 			if err != nil {
-// 				return err
-// 			}
-
-// 			if len(baseDir) > 0 {
-// 				header.Name = filepath.Join(baseDir, strings.TrimPrefix(path, fullpath))
-// 			}
-
-// 			if info.IsDir() {
-// 				header.Name += "/"
-// 			} else {
-// 				header.Method = zip.Deflate
-// 			}
-
-// 			writer, err := archive.CreateHeader(header)
-// 			if err != nil {
-// 				return err
-// 			}
-
-// 			if info.IsDir() {
-// 				return nil
-// 			}
-
-// 			file, err := os.Open(path)
-// 			if err != nil {
-// 				return err
-// 			}
-// 			defer file.Close()
-// 			_, err = io.Copy(writer, file)
-// 			return err
-// 		})
-// 	}
-
-// 	return nil
-// }
--- service/network/sync.go
+++ service/network/sync.go
@@ -1,9 +1,5 @@
 package network
 
-import "github.com/ratel-drive-core/common/util/config"
-
-var storageConfig = config.GetStorageConfig()
-
 // synchronize network infomation such as machine IP, services ports,
 // and other necessary information to the central server.
 func SyncNetworkInfoRequest() {
